2947bdfaba45c2925a81c7faa3c6396915d323f7
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2023-02-26 20:48:58 +0000

    Expand example

--- src/lib/server/router/example.ts
+++ src/lib/server/router/example.ts
@@ -4,10 +4,17 @@ import { z } from 'zod';
 
 export const t = initTRPC.context<Context>().create();
 
+let name = "Enter your name"
+
 export const exampleRouter = t.router({
-	greeting: t.procedure.input(z.object({ name: z.string() })).query(async ({ ctx, input }) => {
+	greeting: t.procedure.input(z.object({  })).query(async ({ ctx }) => {
 		await new Promise((resolve) => setTimeout(resolve, 3000)); // 👈 simulate an expensive operation
-		return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()} / ${ctx.example} ${input.name}`;
+		return `Hello from tRPC @ ${new Date().toLocaleTimeString()} / ${ctx.example} \n Name: ${name}`;
+	}),
+
+  setName: t.procedure.input(z.object({ name: z.string().max(10) })).mutation(async ({ ctx, input }) => {
+		await new Promise((resolve) => setTimeout(resolve, 3000)); // 👈 simulate an expensive operation
+    name = input.name
 	})
 });
 
