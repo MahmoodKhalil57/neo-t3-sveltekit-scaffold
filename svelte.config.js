7d3cc53425df05353f98fed83b7932c8832aab73
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2023-02-26 00:41:59 +0000

    PWA init

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,33 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		preprocess({
+			sourceMap: true
+		})
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: true,
+			strict: true
+		})
+	},
+
+	vitePlugin: {
+		experimental: {
+			inspector: true
+		}
 	}
 };
 
