b245a998619cc07e6cd588ce2dabb77743b28aa0
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2023-02-26 02:53:56 +0000

    init Tailwind Daisy UI

--- src/lib/deleteThisFolder/CssShowcase.svelte
+++ src/lib/deleteThisFolder/CssShowcase.svelte
@@ -1,5 +1,12 @@
 <div class="outer">
-	<div class="notConnected">No Css Framework Connected</div>
+	<div class="notConnected !hidden">No Css Framework Connected</div>
+	<div class="Connected text-2xl text-green-600 font-[800] !flex flex-row gap-3">
+		<div class="i-material-symbols-check-circle-rounded" />
+		<div class="tooltip tooltip-open" data-tip="+DaisyUi">
+			<div>Tailwind Connected Successfully</div>
+		</div>
+		<div class="i-noto-party-popper" />
+	</div>
 </div>
 
 <style>
@@ -21,5 +28,8 @@
 		color: rgb(184, 11, 11);
 		font-size: x-large;
 		font-weight: 800;
+	}
+	.Connected {
+		display: none;
 	}
 </style>
