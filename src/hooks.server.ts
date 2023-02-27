436e54faa935f74fa5ab741603e6283646083b2a
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2023-02-27 00:44:01 +0000

    init Authjs

---
+++ src/hooks.server.ts
@@ -0,0 +1,13 @@
+import type { Handle } from '@sveltejs/kit';
+import { SvelteKitAuth } from "@auth/sveltekit"
+import GoogleProvider from "@auth/core/providers/google"
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// Not sure why I cant get types on env
+import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
+
+export const handle: Handle = SvelteKitAuth({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+ providers: [GoogleProvider({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
+})
