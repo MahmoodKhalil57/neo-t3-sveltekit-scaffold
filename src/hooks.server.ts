fb099fd8ad1e47ded3e8acf329b61810e6aee5e5
MahmoodKhalil57 <46604753+MahmoodKhalil57@users.noreply.github.com>
2023-02-27 01:56:23 +0000

    init prisma Authjs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,15 @@
 import type { Handle } from '@sveltejs/kit';
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GoogleProvider from "@auth/core/providers/google"
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '$lib/server/prismaClient';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // Not sure why I cant get types on env
 import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
 
 export const handle: Handle = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
  providers: [GoogleProvider({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
